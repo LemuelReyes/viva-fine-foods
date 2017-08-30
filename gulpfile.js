7915fbe60d3b4934139da67f05ada5235e29ef35
Lemuel <reyeslemuel@hotmail.com>
2017-08-30 16:05:02 -0700

    organize folders and files. add dependencies and construct a simple basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+
+gulp.task('default', function(){
+  console.log("hooray");
+});
+
+gulp.task('html', function(){
+  console.log("HTML WOOO");
+});
+
+gulp.task('styles', function(){
+  console.log("imagine css here!");
+});
+
+gulp.task('watch', function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+});
