ffbd5fc52199bd57f54f0ef11347a2187466bc17
Lemuel <reyeslemuelj@gmail.com>
2018-04-02 05:56:01 -0700

    add google maps and contact page

---
+++ app/assets/scripts/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 49.272037, lng: -123.155166};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
