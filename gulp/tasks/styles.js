267ca6187926ed01bd6630614cbb7751b42bfb0a
Lemuel <reyeslemuel@hotmail.com>
2018-04-04 21:17:16 -0700

    tablet bp. slight changes in paragraph widths, resizing banners

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
+cssExtend = require('postcss-extend'),
+sass = require('gulp-sass'),
 mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, cssExtend, sass, autoprefixer]))
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end');
