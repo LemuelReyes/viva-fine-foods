3c6cd325709454a25535d5d7e8c27fbcdfd99a8b
Lemuel <reyeslemuel@hotmail.com>
2018-05-01 03:10:55 -0700

    mstyle obile and desktop dropdown menu

---
+++ app/assets/scripts/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("hamburger-icon").style.display = "none";
+  document.getElementById("nav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  document.getElementById("hamburger-icon").style.display = "block";
+}
