8f34b5bdaeffea38bc80c74db063ed6ce517cc7e
Lemuel <reyeslemuel@hotmail.com>
2018-05-03 05:27:25 -0700

    create build file tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,25 +1,34 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync =  require('browser-sync').create();
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "docs"
+      baseDir: "app"
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
-  watch('./app/assets/styles/**/*.css', function(){
+
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
+  });
+
+  watch('./app/assets/scripts/**/*.js', function(){
+    gulp.start('scriptsRefresh');
   });
 });
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/style.css')
-  .pipe(browserSync.stream());
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload()
 });
